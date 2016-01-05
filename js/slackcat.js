065365ffc8a0581cd568a6e44bef9c2da03692c8
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-01-05 00:21:02 -0500

    fix indent, update logo

--- js/slackcat.js
+++ js/slackcat.js
@@ -14,34 +14,34 @@ function paramToArray(prmstr) {
 }
 
 function failed(msg) {
-    var h = document.createElement('h1');
-		h.innerHTML = msg;
-		$('#token-box').append(h);
+  var h = document.createElement('h1');
+  h.innerHTML = msg;
+  $('#token-box').append(h);
 }
 
 $(document).ready(function() {
   var params = getParameters();
-	if (jQuery.isEmptyObject(params)) {
-		failed('something broke');
-	} else if ("error" in params) {
-		failed('Slack authentication error: ' + params.error);
-	} else {
+  if (jQuery.isEmptyObject(params)) {
+    failed('something broke');
+  } else if ("error" in params) {
+    failed('Slack authentication error: ' + params.error);
+  } else {
     $.ajax("http://api.vektor.nyc/slackcat", {
       method: "POST",
       data: params,
       error: function(xhr, status) {
         console.log(xhr.status);
-				failed();
+        failed();
       }
     })
-			.done(function(token) {
-        var t1 = document.createElement('h2')
-            t2 = document.createElement('h1')
-				t1.innerHTML = 'use the below token to complete Slackcat setup:'
-				t2.innerHTML = token
-				$('#token-box').append(t1);
-				$('#token-box').append(t2);
-			});
-	}
-	console.log(params);
+    .done(function(token) {
+      var t1 = document.createElement('h2')
+        t2 = document.createElement('h1')
+        t1.innerHTML = 'use the below token to complete Slackcat setup:'
+        t2.innerHTML = token
+        $('#token-box').append(t1);
+      $('#token-box').append(t2);
+    });
+  }
+  console.log(params);
 });
